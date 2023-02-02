876d70a0e836a8ab02ca1ab44ba9a64c0f3e5654
mf1031-vvquyet <vuvanquyet17102@gmail.com>
2023-02-02 22:42:41 +0700

    publish "Microsoft.AspNetCore.WebHooks.Receivers" first time

--- src/Microsoft.AspNetCore.WebHooks.Receivers.Xsolla/Properties/Resources.Designer.cs
+++ src/Microsoft.AspNetCore.WebHooks.Receivers.Xsolla/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Microsoft.AspNetCore.WebHooks.Receivers.Xsolla.Properties {
+namespace Microsoft.AspNetCore.WebHooks.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Microsoft.AspNetCore.WebHooks.Receivers.Xsolla.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.AspNetCore.WebHooks.Receivers.Xsolla.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.AspNetCore.WebHooks.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
